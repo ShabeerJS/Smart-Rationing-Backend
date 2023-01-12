f2b2469e42a98b3a4ec86fa949d9338ebd600539
Shabeer Jaffer <shabeerj@stg.com>
2023-01-12 11:12:57 +0530

    first commit

--- src/main/java/com/stg/serviceImpl/RationAdminImpl.java
+++ src/main/java/com/stg/serviceImpl/RationAdminImpl.java
@@ -49,18 +49,17 @@ public class RationAdminImpl implements RationAdminService {
 	private RationCardMemberRepository rationCardMemberRepository;
 
 	// Ration Admin/
-	
+
 	@Override
 	public RationAdmin rationAdminLogin(String emailId) {
-		String messege="";
-		RationAdmin admin=null;
-		   RationAdmin rationAdminGet  = rationAdminRepository.findByEmailId(emailId);
-		        if(rationAdminGet.getEmailId().equals(emailId)) {
-		        	admin=rationAdminGet;
-		        }
-		        else {
-		        	throw new RationApplicationException("You have entered Wrong Mail Id");
-		        }
+		String messege = "";
+		RationAdmin admin = null;
+		RationAdmin rationAdminGet = rationAdminRepository.findByEmailId(emailId);
+		if (rationAdminGet.getEmailId().equals(emailId)) {
+			admin = rationAdminGet;
+		} else {
+			throw new RationApplicationException("You have entered Wrong Mail Id");
+		}
 		return admin;
 	}
 
@@ -137,7 +136,7 @@ public class RationAdminImpl implements RationAdminService {
 //		RationAdmin rationAdmin = rationAdminRepository.findById(rationAdminId).get();
 //		if (rationAdminRepository.existsById(rationAdminId)) {
 
-			return orderRepository.findAll();
+		return orderRepository.findAll();
 
 //		} else {
 //			throw new RationApplicationException(rationAdminId + " Given RationAdmin Id is Not Found");
@@ -147,12 +146,12 @@ public class RationAdminImpl implements RationAdminService {
 
 	@Override
 	public OrderDetails readOrders(int orderId) throws RationApplicationException {
-		
+
 		OrderDetails orderDetails = orderRepository.findById(orderId).get();
 		return orderDetails;
 	}
 
-	//=============================================================================
+	// =============================================================================
 	// RationAdmin---------RationCardUsers//
 	@Override
 	public List<RationCard> readUsers(int rationAdminId) throws RationApplicationException {
@@ -171,52 +170,51 @@ public class RationAdminImpl implements RationAdminService {
 	// RationAdmin---------------Product//
 	@Override
 	public List<Product> readProduct() throws RationApplicationException {
-		
-		
+//		List<Product> list = new ArrayList<Product>();
+//		if (rationAdminRepository.existsById(rationAdminId)) {
+//			List<Product> listAll = productRepository.findAll();
+//			for (Product product : listAll) {
+//				if (product.getRationAdmin().getRationAdminId() == rationAdminId) {
+//					list.add(product);
+//				}
+//			}
+//		}
+		return productRepository.findAll();
 
-			return  productRepository.findAll();
-
-		} 
+	}
 
 	@Override
 	public Product readByIdProducts(int productId) throws RationApplicationException {
-		       Product product=  productRepository.findById(productId).get();
+		Product product = productRepository.findById(productId).get();
 		return product;
 	}
 
-	
 	@Override
 	public Product updateProduct(int productId, Product product) throws RationApplicationException {
-		
-		Product product2 = productRepository.findById(productId).orElseThrow(()->new RationApplicationException("Not Found"));
-       product2.setProductName(product.getProductName());
-       product2.setProductPrice(product.getProductPrice());
-       product2.setQuantity(product.getQuantity());
 
-        
+		Product product2 = productRepository.findById(productId)
+				.orElseThrow(() -> new RationApplicationException("Not Found"));
+		product2.setProductName(product.getProductName());
+		product2.setProductPrice(product.getProductPrice());
+		product2.setQuantity(product.getQuantity());
 
-       Product updatedproduct = productRepository.save(product2);
-        return updatedproduct;
-    }
+		Product updatedproduct = productRepository.save(product2);
+		return updatedproduct;
+	}
 
 	@Override
-	public Product addProducts( Product products,int rationAdminId) throws RationApplicationException {
+	public Product addProducts(Product products, int rationAdminId) throws RationApplicationException {
 //		RationAdmin rationAdmin = rationAdminRepository.findById(rationAdminId).get();
 //		products.setRationAdmin(rationAdmin);
-		
-		RationAdmin rationAdmin=rationAdminRepository.findById(rationAdminId).get();
+
+		RationAdmin rationAdmin = rationAdminRepository.findById(rationAdminId).get();
 		products.setRationAdmin(rationAdmin);
-	
-		
-		
+
 		return productRepository.save(products);
 	}
 
-	
-	
-	
 	@Override
-	public String deleteProduct( int productId) throws RationApplicationException {
+	public String deleteProduct(int productId) throws RationApplicationException {
 
 		if (productRepository.existsById(productId)) {
 			productRepository.deleteById(productId);
@@ -226,7 +224,7 @@ public class RationAdminImpl implements RationAdminService {
 			throw new RationApplicationException("product Id is incorrect");
 		}
 	}
-	//========================================================================
+	// ========================================================================
 
 //	@Override
 //	public Product updateProduct(int rationAdminId, int productId, int quantity) throws RationApplicationException {
@@ -274,29 +272,35 @@ public class RationAdminImpl implements RationAdminService {
 //			throw new RationApplicationException("can't able to create");
 //		}
 //	}
-	
+
 	@Override
-	public RationCardMember addRationCardMember(RationCardMember rationCardMember,int rationId)
+	public RationCardMember addRationCardMember(RationCardMember rationCardMember, int rationId)
 			throws RationApplicationException {
-	RationCard rationCard=	rationCardRepository.findById(rationId).get();
-		                        rationCardMember.setRationCard(rationCard);
-		                       
-			return rationCardMemberRepository.save(rationCardMember);
-		} 
+		RationCard rationCard = rationCardRepository.findById(rationId).get();
+		rationCardMember.setRationCard(rationCard);
+
+		return rationCardMemberRepository.save(rationCardMember);
+	}
 
 	// Ration Card User====================================
 	@Override
 	public List<RationCard> readRationCard() throws RationApplicationException {
-
+//		List<RationCard> list = new ArrayList<RationCard>();
+//
 //		RationAdmin rationAdmin = rationAdminRepository.findById(rationAdminId).get();
 //		if (rationAdminRepository.existsById(rationAdminId)) {
-
-			return rationCardRepository.findAll();
-	}
-
+//
+//			List<RationCard> listAll = rationCardRepository.findAll();
+//			for (RationCard rationCard : listAll) {
+//				if (rationCard.getRationAdmin().getRationAdminId() == rationAdminId) {
+//					list.add(rationCard);
+//				}
+//			}
 //		} else {
 //			throw new RationApplicationException(rationAdminId + "You Have Entered Wrong  rationAdminId ");
 //		}
+		return rationCardRepository.findAll();
+	}
 
 	@Override
 	public RationCard readRationCard(long rationCardNo) throws RationApplicationException {
@@ -305,11 +309,11 @@ public class RationAdminImpl implements RationAdminService {
 	}
 
 	@Override
-	public RationCard addRationCard(RationCard rationCard,int rationAdminId ) throws RationApplicationException {
-		
-		RationAdmin rationAdmin=rationAdminRepository.findById(rationAdminId).get();
+	public RationCard addRationCard(RationCard rationCard, int rationAdminId) throws RationApplicationException {
+
+		RationAdmin rationAdmin = rationAdminRepository.findById(rationAdminId).get();
 		rationCard.setRationAdmin(rationAdmin);
-	
+
 		return rationCardRepository.save(rationCard);
 //		RationAdmin rationAdmin = rationAdminRepository.findById(rationAdminId).get();
 //		List<FixedProducts> fixedProducts=fixedProductRepository.findAll();
@@ -330,28 +334,26 @@ public class RationAdminImpl implements RationAdminService {
 //	
 //			Address address = rationCard.getAddresses();
 //			if (address.getArea().equalsIgnoreCase(area)) {
-	
+
 	@Override
-	public String deleteRationCard( long rationCardNo) throws RationApplicationException {
+	public String deleteRationCard(long rationCardNo) throws RationApplicationException {
 //		RationAdmin rationAdmin = rationAdminRepository.findById(rationAdminId).get();
 //		if (rationAdminRepository.existsById(rationAdminId)) {
 
-			RationCard rationCardUser = rationCardRepository.findByRationCardNo(rationCardNo);
-			if (rationCardUser.getRationCardNo() == rationCardNo) {
-				rationCardRepository.delete(rationCardUser);
-				return "\"RationCard Deleted successfully\"";
-				
-			} else {
-				throw new RationApplicationException(rationCardNo + "You Have Entered Wrong RationCard No");
-			}
+		RationCard rationCardUser = rationCardRepository.findByRationCardNo(rationCardNo);
+		if (rationCardUser.getRationCardNo() == rationCardNo) {
+			rationCardRepository.delete(rationCardUser);
+			return "\"RationCard Deleted successfully\"";
+
+		} else {
+			throw new RationApplicationException(rationCardNo + "You Have Entered Wrong RationCard No");
 		}
+	}
 
 //		else {
 //
 //			throw new RationApplicationException(rationAdminId + " Given rationAdmin Id is Not Found");
 //		}
-	
-
 
 //=================================================================================
 	@Override
@@ -362,14 +364,14 @@ public class RationAdminImpl implements RationAdminService {
 	}
 
 	@Override
-	public String UpdateOrderStatus(int orderId,OrderStatus orderStatus) throws RationApplicationException {
+	public String UpdateOrderStatus(int orderId, OrderStatus orderStatus) throws RationApplicationException {
 		OrderDetails orderDetails1 = orderRepository.findById(orderId).get();
-		 {
+		{
 			orderDetails1.setOrderStatus(orderStatus);
 			orderRepository.save(orderDetails1);
-		
+
 			return "\"Order Status Updated Successfully\"";
-		} 
+		}
 
 	}
 
@@ -386,12 +388,11 @@ public class RationAdminImpl implements RationAdminService {
 		}
 	}
 
-	
-	//==========================Products=============================
+	// ==========================Products=============================
 
-	//====================================FixedProducts=================
+	// ====================================FixedProducts=================
 	@Override
-	public FixedProducts addFixedProducts( FixedProducts fixedProducts,int rationAdminId)
+	public FixedProducts addFixedProducts(FixedProducts fixedProducts, int rationAdminId)
 			throws RationApplicationException {
 //		RationAdmin rationAdmin = rationAdminRepository.findById(rationAdminId).get();
 //		//List<RationCard> rationCards = rationCardRepository.findAll();
@@ -406,45 +407,50 @@ public class RationAdminImpl implements RationAdminService {
 //				}
 //			}
 //			fixedProducts.setCards(list);
-		RationAdmin rationAdmin=rationAdminRepository.findById(rationAdminId).get();
+		RationAdmin rationAdmin = rationAdminRepository.findById(rationAdminId).get();
 		fixedProducts.setRationAdmin(rationAdmin);
-	
-			return fixedProductRepository.save(fixedProducts);
+
+		return fixedProductRepository.save(fixedProducts);
 	}
 //		} else {
 //			throw new RationApplicationException("Admin Id is incorrect");
 //		}
 //	}
 
-	
 	@Override
 	public List<FixedProducts> readFixedProduct() throws RationApplicationException {
-		
-		    List<FixedProducts> fixedProducts=  fixedProductRepository.findAll();
-		    if(fixedProducts.isEmpty()) {
-		    	throw new RationApplicationException("empty");
-		    }
-		return fixedProducts;
-	}
+//		List<FixedProducts> list = new ArrayList<FixedProducts>();
+//		if (rationAdminRepository.existsById(rationAdminId)) {
+//			List<FixedProducts> fixedProducts = fixedProductRepository.findAll();
+//			for (FixedProducts fixedProduct : fixedProducts) {
+//				if (fixedProduct.getRationAdmin().getRationAdminId() == rationAdminId) {
+//					list.add(fixedProduct);
+//				}
+//			}
+//		}
 
+//		    if(fixedProducts.isEmpty()) {
+//		    	throw new RationApplicationException("empty");
+//		    }
+		return fixedProductRepository.findAll();
+	}
 
 	@Override
 	public FixedProducts updateFixedProduct(int productId, FixedProducts fixedProducts) {
-		FixedProducts fixedProductsNew= fixedProductRepository.findById(productId).orElseThrow(()->new RationApplicationException("Not Found"));
+		FixedProducts fixedProductsNew = fixedProductRepository.findById(productId)
+				.orElseThrow(() -> new RationApplicationException("Not Found"));
 		fixedProductsNew.setProductName(fixedProducts.getProductName());
 		fixedProductsNew.setProductPrice(fixedProducts.getProductPrice());
 		fixedProductsNew.setProductQuantity(fixedProducts.getProductQuantity());
 		fixedProductsNew.setCardType(fixedProducts.getCardType());
-	        
 
-        FixedProducts updatedFixedproduct = fixedProductRepository.save(fixedProductsNew);
-        return updatedFixedproduct;
-	    }
+		FixedProducts updatedFixedproduct = fixedProductRepository.save(fixedProductsNew);
+		return updatedFixedproduct;
+	}
 
-	
 	@Override
 	@Cascade(CascadeType.DELETE)
-	public String deleteFixedProduct( int productId) throws RationApplicationException {
+	public String deleteFixedProduct(int productId) throws RationApplicationException {
 		if (fixedProductRepository.existsById(productId)) {
 			fixedProductRepository.deleteById(productId);
 			return "\"Deleted successfully\"";
@@ -453,32 +459,30 @@ public class RationAdminImpl implements RationAdminService {
 			throw new RationApplicationException("product Id is incorrect");
 		}
 	}
+
 	@Override
 	public FixedProducts readByIdFixedProducts(int productId) throws RationApplicationException {
-		 FixedProducts fixedProducts=  fixedProductRepository.findById(productId).get();
-			return fixedProducts;
-	
+		FixedProducts fixedProducts = fixedProductRepository.findById(productId).get();
+		return fixedProducts;
+
 	}
 //================================================================================
-	
 
 	@Override
-	public Address addAddress(Address address,int rationId) throws RationApplicationException {
+	public Address addAddress(Address address, int rationId) throws RationApplicationException {
 		RationCard rationCard2 = rationCardRepository.findById(rationId).get();
-	
+
 //			rationCard2.setAddresses(address);
-			address.setRationCard(rationCard2);
+		address.setRationCard(rationCard2);
 //			rationCardRepository.save(rationCard2);
 
-			return addressRepository.save(address);
+		return addressRepository.save(address);
 	}
-	
+
 //		} else {
 //			throw new RationApplicationException("New address Can't able to create");
 //		}
 //	}
-
-	
 
 	@Override
 	public List<OrderDetails> readOrderBymonth(int month) throws RationApplicationException {
@@ -501,25 +505,20 @@ public class RationAdminImpl implements RationAdminService {
 	@Override
 	public List<RationCardMember> addListRationCardMember(List<RationCardMember> rationCardMember, long rationCardNo)
 			throws RationApplicationException {
-		
-		
+
 		RationCard rationCard = rationCardRepository.findByRationCardNo(rationCardNo);
-		List<RationCardMember> rationCardMembers=new ArrayList<RationCardMember>();
-		
+		List<RationCardMember> rationCardMembers = new ArrayList<RationCardMember>();
+
 		for (RationCardMember rationCardMember2 : rationCardMember) {
 			rationCardMember2.setRationCard(rationCard);
 		}
 		if (rationCardMember != null) {
-			//rationCardMember2.setRationCard(rationCard);
+			// rationCardMember2.setRationCard(rationCard);
 			return rationCardMemberRepository.saveAll(rationCardMember);
 		} else {
 			throw new RationApplicationException("can't able to create");
 		}
-	
-		   
-	
-		
-		
+
 	}
 
 	@Override
@@ -527,26 +526,18 @@ public class RationAdminImpl implements RationAdminService {
 			throws RationApplicationException {
 
 		RationCard rationCard = rationCardRepository.findById(rationId).get();
-		List<RationCardMember> rationCardMembers=new ArrayList<RationCardMember>();
-		
+		List<RationCardMember> rationCardMembers = new ArrayList<RationCardMember>();
+
 		for (RationCardMember rationCardMember2 : rationCardMember) {
 			rationCardMember2.setRationCard(rationCard);
 		}
 		if (rationCardMember != null) {
-			//rationCardMember2.setRationCard(rationCard);
+			// rationCardMember2.setRationCard(rationCard);
 			return rationCardMemberRepository.saveAll(rationCardMember);
 		} else {
 			throw new RationApplicationException("can't able to create");
 		}
-	
+
 	}
-	
 
-	
-	
-
-	
-
-	
-	
 }
